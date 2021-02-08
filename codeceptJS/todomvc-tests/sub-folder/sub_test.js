d0236b80c061fe1d294f4443a663a1dd98028d47
koushikmohan1996 <koushikmohan1996@gmail.com>
2021-02-08 16:42:17 +0530

    Add sub tests

---
+++ codeceptJS/todomvc-tests/sub-folder/sub_test.js
@@ -0,0 +1,36 @@
+Feature('Sub test @S516be4f0')
+
+Before(async ({ I, TodosPage }) => {
+  TodosPage.goto()
+});
+
+/**
+ * Happy Path tests
+ */
+Scenario('Create a new sub test @Tec5c27c1', async ({ I, TodosPage }) => {
+  I.say('Given I have an empty todo list')
+
+  I.say('When I create a todo "foo"')
+  TodosPage.enterTodo('foo')
+
+  I.say('Then I see the new todo on my list')
+  TodosPage.seeNumberOfTodos(1)
+
+  I.saveScreenshot('create-todo-item.png')
+});
+
+Scenario('Create multiple subt test @T344e7d0a', async ({ I, TodosPage }) => {
+  I.say('Given I have an empty todo list')
+  I.say('When I create todos "foo", "bar" and "baz"')
+  TodosPage.enterTodo('foo')
+  TodosPage.enterTodo('bar')
+  TodosPage.enterTodo('baz')
+
+  I.say('Then I have these 3 todos on my list')
+  TodosPage.seeNumberOfTodos(3)
+
+  I.saveScreenshot('create-multiple-todo-items.png')
+})
+
+
+
